e649d61a82afc0089749b27454fc2afcd1c29670
Sundeep Ammisetti <sundeepammisetti@gmail.com>
2020-03-18 14:31:53 +0530

    Http Services Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpService } from './http.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HttpServices';
+  public persondata = [];
+  constructor(private httpservice: HttpService) { }
+  ngOnInit() {
+    this.httpservice.getData().subscribe((data)=> {
+      this.persondata = Array.from(Object.keys(data), k=>data[k]);
+      console.log(this.persondata);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpService } from './http.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +9,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
